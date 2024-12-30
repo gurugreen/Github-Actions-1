64442d3bef6557ea229030bfcbcf96d4843f8678
gurugreen <guruprasanna.green@gmail.com>
2024-12-31 00:37:48 +0530

    updated terraform.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
