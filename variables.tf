cafcee1d7a12e1cd74e56af14e231c8275576a6d
gurugreen <guruprasanna.green@gmail.com>
2024-12-31 00:42:49 +0530

    removed aws credentials

--- variables.tf
+++ variables.tf
@@ -8,12 +8,12 @@ variable "bucket_name" {
   type        = string
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  description = "The name of the S3 bucket"
-  type        = string
-}
+# variable "AWS_ACCESS_KEY_ID" {
+#   description = "The name of the S3 bucket"
+#   type        = string
+# }
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "The name of the S3 bucket"
-  type        = string
-}
+# variable "AWS_SECRET_ACCESS_KEY" {
+#   description = "The name of the S3 bucket"
+#   type        = string
+# }
